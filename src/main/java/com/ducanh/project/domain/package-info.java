496c5a33d6c7054f40ae9315b3927e80e0a47cee
DESKTOP-7RHA4DM\Duc Anh <ducanhle4596@gmail.com>
2018-03-31 10:35:25 +0700

    Initial application generated by JHipster-4.14.0

---
+++ src/main/java/com/ducanh/project/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.ducanh.project.domain;
---
+++ src/main/webapp/app/account/account.state.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('vnLoversApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('account', {
+            abstract: true,
+            parent: 'app'
+        });
+    }
+})();
