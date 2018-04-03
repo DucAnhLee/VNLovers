73362f54b89900085028932f3f497f03e5cb72db
DESKTOP-7RHA4DM\Duc Anh <ducanhle4596@gmail.com>
2018-04-03 07:16:28 +0700

    edit home page

--- src/main/java/com/ducanh/project/domain/VnLoverHome.java
+++ src/main/java/com/ducanh/project/domain/VnLoverHome.java
@@ -12,12 +12,18 @@ public class VnLoverHome implements Serializable{
 	private VNLoverRating rating;
 	
 	private List<List<Long>> post_timestamp;
+	
+	private List<List<Long>> post_per_hours;
+	
+	private List<List<String>> top10_locations;;
 
-	public VnLoverHome(VNLoverInfo info, VNLoverRating rating, List<List<Long>> post_timestamp) {
+	public VnLoverHome(VNLoverInfo info, VNLoverRating rating, List<List<Long>> post_timestamp, List<List<Long>> post_per_hours, List<List<String>> top10_locations) {
 		super();
 		this.info = info;
 		this.rating = rating;
 		this.post_timestamp = post_timestamp;
+		this.post_per_hours = post_per_hours;
+		this.top10_locations = top10_locations;
 	}
 
 	public VNLoverInfo getInfo() {
@@ -42,6 +48,22 @@ public class VnLoverHome implements Serializable{
 
 	public void setPost_timestamp(List<List<Long>> post_timestamp) {
 		this.post_timestamp = post_timestamp;
+	}
+
+	public List<List<Long>> getPost_per_hours() {
+		return post_per_hours;
+	}
+
+	public void setPost_per_hours(List<List<Long>> post_per_hours) {
+		this.post_per_hours = post_per_hours;
+	}
+
+	public List<List<String>> getTop10_locations() {
+		return top10_locations;
+	}
+
+	public void setTop10_locations(List<List<String>> top10_locations) {
+		this.top10_locations = top10_locations;
 	}
 	
 }
